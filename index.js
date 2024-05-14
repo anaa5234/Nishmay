6750a481b46a4b13a7779e58eb99a54a2dc24b29
Ishank Lalwani <ishanklalwani@gmail.com>
2024-05-15 02:52:06 +0530

    added the blogs page

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const slides = document.querySelectorAll('.slide');
-let index = 0;
+const carousel = document.querySelector('.carousel-inner');
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
 
-function prevSlide(){
-    slides[index].classList.remove('active');
-    index--;
-
-    if(index < 0)
-        index = slides.length -1;
-
-    slides[index].classList.add('active');      
+function showSlide(index) {
+  carouselItems.forEach(item => item.classList.remove('active'));
+  carouselItems[index].classList.add('active');
 }
 
-document.querySelector('.prev').addEventListener('click', e => {
-    prevSlide();
-});
-
-function nextSlide(){
-    slides[index].classList.remove('active');
-    index++;
-
-    if(index > slides.length -1)
-        index = 0;
-
-    slides[index].classList.add('active');      
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  showSlide(currentIndex);
 }
 
-document.querySelector('.next').addEventListener('click', e => {
-    nextSlide();
-});
+setInterval(nextSlide, 5000);
